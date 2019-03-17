fdb549474980babe04d922478694dc0815b7a7c5
Phil Maurer <philmau@gmail.com>
2019-03-17 22:43:34 +0000

    Updates

---
+++ precache-manifest.871e96b100624cdf66ec04bc0ba9c60e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaff2bcb04a2fb6176ba",
+    "url": "/tailor-colors/static/css/main.f11bd5e0.chunk.css"
+  },
+  {
+    "revision": "eaff2bcb04a2fb6176ba",
+    "url": "/tailor-colors/static/js/main.eaff2bcb.chunk.js"
+  },
+  {
+    "revision": "80ec22804bb21ac709db",
+    "url": "/tailor-colors/static/js/runtime~main.80ec2280.js"
+  },
+  {
+    "revision": "3210e2b17ad6e210f486",
+    "url": "/tailor-colors/static/js/2.3210e2b1.chunk.js"
+  },
+  {
+    "revision": "fa57d11cd80e3df10c0e9c4875fa1fa6",
+    "url": "/tailor-colors/index.html"
+  }
+];
