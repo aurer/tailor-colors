9fd02e0cf17a0eea29ebacb319f0f4a41981d08b
Phil Maurer <philmau@gmail.com>
2020-12-21 21:07:33 +0000

    Add sentry error tracking

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ErrorBoundary } from '@sentry/react'
 import ColorGroups from './ColorGroups'
 import Menu from './Menu'
 import { ColorProvider } from './ColorContext'
@@ -6,16 +7,18 @@ import '../styles/App.scss'
 
 const App = () => {
 	return (
-		<ColorProvider>
-			<div className="App">
-				<div className="App-main">
-					<ColorGroups />
+		<ErrorBoundary>
+			<ColorProvider>
+				<div className="App">
+					<div className="App-main">
+						<ColorGroups />
+					</div>
+					<div className="App-menu">
+						<Menu />
+					</div>
 				</div>
-				<div className="App-menu">
-					<Menu />
-				</div>
-			</div>
-		</ColorProvider>
+			</ColorProvider>
+		</ErrorBoundary>
 	)
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import * as Sentry from '@sentry/react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Sentry.init({
+	dsn: 'https://534366c7cbb145b6a078617778401236@o252569.ingest.sentry.io/5566300',
+	environment: process.env.NODE_ENV,
+	release: `huely@${process.env.REACT_APP_VERSION}`,
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
